6c3d05e93e26954a1ee83b500c6289b978c17083
HasibShafi1 <hasibsafi.h3@gmail.com>
2025-11-29 23:30:11 +0600

    for github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: Replace 'academiml' with your actual repository name
+  // If your repo is https://github.com/user/my-project, this should be '/my-project/'
+  base: '/academiml/', 
 })
